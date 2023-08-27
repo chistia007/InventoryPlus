2a2aea8140e5e411c5854e585a65c17c3b1ff9be
Chisthia Khan <chisti.khan1999@gmail.com>
2023-08-27 20:02:39 +0600

    v8

--- src/main/resources/static/ownerTab.js
+++ src/main/resources/static/ownerTab.js
@@ -15,7 +15,6 @@
         })
        .then(response => response.text())
        .then(data => {
-           // Display the response message in userInfoContainer-->
            location.reload();
         })
         .catch(error => console.error('Error:', error));
@@ -23,19 +22,15 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Get modal elements
     const addUserModal = document.getElementById("addUserModal");
     const closeModalButton = document.getElementById("closeModal");
 
-    // Get the "Add User" button
     const addUserButton = document.getElementById("addUserButton");
 
-    // When the "Add User" button is clicked, open the modal
     addUserButton.addEventListener("click", function () {
         addUserModal.style.display = "block";
     });
 
-    // When the close button in the modal is clicked, close the modal
     closeModalButton.addEventListener("click", function () {
         addUserModal.style.display = "none";
     });
@@ -45,18 +40,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the user list element
     const userList = document.getElementById("userList");
 
-    // Function to load users from the API
     function loadUsers() {
-        fetch("/bending/getUsers") // Replace with the actual URL of your API endpoint
+        fetch("/bending/getUsers")
             .then((response) => response.json())
             .then((data) => {
-                // Clear existing list items
+
                 userList.innerHTML = "";
 
-                // Create list items for each user and append to the list
                 data.forEach((user) => {
                     const listItem = document.createElement("li");
                     listItem.textContent = `User Name: ${user.userName}, User Type: ${user.userType}, Salary: ${user.salary}`;
@@ -68,13 +60,11 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     }
 
-    // Load users when the page loads
     loadUsers();
 });
 
 
 
- //Adding  event listener for delete user button
 document.getElementById("deleteUserButton").addEventListener("click", function(event) {
     const username = document.getElementById('deleteUserName').value; // You need to get the value of the input field
 
--- src/main/resources/static/plot_info.js
+++ src/main/resources/static/plot_info.js
@@ -5,36 +5,30 @@ const searchResults = document.getElementById('search-results');
 searchInput.addEventListener('input', function () {
     const query = searchInput.value.trim();
 
-    if (query === '') {                              // Check if the query is empty, and if so, clear the search results
+    if (query === '') {
         searchResults.innerHTML = '';
-        return; // Exit the function early
+        return;
     }
 
-    // Make an AJAX request to your Java servlet with the correct context path
     console.log('Query:', query);
     fetch('/bending/search?query=' + query)
         .then(response => response.json())
         .then(data => {
-            // Clear previous search results
             searchResults.innerHTML = '';
             console.log('Data:', data);
 
-            // Display search results if there are any
             if (data.length > 0) {
                 data.forEach(item => {
                     const listItem = document.createElement('li');
                     listItem.textContent = `Id: ${item.plot_id},Name: ${item.productName}, Location: ${item.location}, Image URL: ${item.imageUrl}, Total Quantity: ${item.totalQuantity}, Quantity Sold: ${item.quantitySold}, Quantity Left: ${item.quantityLeft}, Moved To Warehouse: ${item.movedToWarehouse}, Product Grade: ${item.productGrade}, Ploughing Time: ${item.ploughingTime}, Reaping Time: ${item.reapingTime}, Warehouse Id: ${item.wareHouseId}`;
 
-                    // Add a click event listener to each search result item
                     listItem.addEventListener('click', () => {
                         displayItemDetails(item);
                     });
 
-                    // Append the list item to the search results
                     searchResults.appendChild(listItem);
                 });
             } else {
-                // Handle case when no results are found
                 searchResults.innerHTML = '<li>No results found</li>';
             }
         })
